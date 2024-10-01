92277d99c9bb2f7d982fdaa82d050a51f5467d3f
Divi Sukumar <154667008+Divisukumar3010@users.noreply.github.com>
2024-10-01 22:24:54 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,25 @@
+public class DataTypes 
+{
+    public static void main(String[] args) {
+        int num = 10;
+        byte b = 60;
+        short s = 558;
+        long l = 100505;
+
+        float f = 5.2f;
+        double d = 2.2456d;
+
+        char c = 'j';
+
+        boolean bool = true;
+        
+        System.out.println("Integer: " + num);
+        System.out.println("Byte: " + b);
+        System.out.println("Short: " + s);
+        System.out.println("Long: " + l);
+        System.out.println("Float: " + f);
+        System.out.println("Double: " + d);
+        System.out.println("Character: " + c);
+        System.out.println("Boolean: " + bool);
+    }
+}
