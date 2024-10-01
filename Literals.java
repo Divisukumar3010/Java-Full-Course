304ab779e0af09fa9797eb1229e1ca7951ccb189
Divi Sukumar <154667008+Divisukumar3010@users.noreply.github.com>
2024-10-01 22:42:54 +0530

    Create Literals.java

---
+++ Literals.java
@@ -0,0 +1,11 @@
+// Literals
+public class Literals{
+    public static void main(String[] args) {
+        int num1 = 0b101;   // 0b is used to convert the number 101 into binary number 
+        int num2 = 0x7E;     // 0x is used to convert the number 1E into hexadecimal number
+        int num3 = 10_00_00_000;  // we can use the underscore for seperating the numbers
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+    }
+}
