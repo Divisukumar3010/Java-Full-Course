d3881af14e425b2dec1ecc10fe1112c7cda8c7d4
Divi Sukumar <154667008+Divisukumar3010@users.noreply.github.com>
2024-10-02 22:14:15 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,11 @@
+public class Arithmetic{
+    public static void main(String args[]){
+        int num1 = 7;
+        int num2 = 5;
+        System.out.println("Addition: "+ (num1+num2));
+        System.out.println("Subtraction: "+ (num1-num2));
+        System.out.println("Multiplication: "+ (num1*num2));
+        System.out.println("Division: "+ (num1/num2));
+        System.out.println("Modulus: "+ (num1%num2));
+    }
+}
