f55efe21f48cdb07a943995a385a1a5b1aec75cd
Divi Sukumar <154667008+Divisukumar3010@users.noreply.github.com>
2024-10-02 22:43:04 +0530

    Add files via upload

---
+++ Relational.java
@@ -0,0 +1,13 @@
+public class Relational
+{
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        System.out.println("a > b: "+ (a>b));
+        System.out.println("a < b: "+ (a<b));
+        System.out.println("a == b: "+ (a==b));
+        System.out.println("a!= b: "+ (a!=b));
+        System.out.println("a >= b: "+ (a>=b));
+        System.out.println("a <= b: "+ (a<=b));
+    }
+}
