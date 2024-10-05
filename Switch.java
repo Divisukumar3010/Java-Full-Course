a76df180d57a40b9e9e80031dc60586e03b908e5
Divi Sukumar <154667008+Divisukumar3010@users.noreply.github.com>
2024-10-05 21:45:00 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,20 @@
+public class Switch{
+    public static void main(String[] args)
+    {
+        int num = 3;
+        switch(num)
+        {
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            default:
+                System.out.println("Number is not found");
+        }
+    }
+}
