b812ed229b77cdb5b9e6fda8b68e3d181cc8e051
Divi Sukumar <154667008+Divisukumar3010@users.noreply.github.com>
2024-10-05 21:26:48 +0530

    Add files via upload

---
+++ If_Else_If.java
@@ -0,0 +1,27 @@
+class If_Else_If
+{
+    public static void main(String[] args)
+    {
+        int marks = 85;
+        if(marks >= 90)
+        {
+            System.out.println("Grade: A");
+        }
+        else if(marks >= 80)
+        {
+            System.out.println("Grade: B");
+        }
+        else if(marks >= 70)
+        {
+            System.out.println("Grade: C");
+        }
+        else if(marks >= 60)
+        {
+            System.out.println("Grade: D");
+        }
+        else
+        {
+            System.out.println("Grade: F");
+        }
+    }
+}
