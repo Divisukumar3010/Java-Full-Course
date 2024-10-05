0e1085ea1842d9162fb43c34d95d9161ae74c52c
Divi Sukumar <154667008+Divisukumar3010@users.noreply.github.com>
2024-10-05 21:41:31 +0530

    Add files via upload

---
+++ Ternary.java
@@ -0,0 +1,10 @@
+class Ternary
+{
+    public static void main(String[] args)
+    {
+        int a =10;
+        
+        String result = a%2==0 ? "even" : "odd";
+        System.out.println(result);
+    }
+}
