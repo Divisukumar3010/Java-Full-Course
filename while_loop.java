05ab1f7747f1881229338355da8937c73cc8d7d7
Divi Sukumar <154667008+Divisukumar3010@users.noreply.github.com>
2024-10-05 22:00:20 +0530

    Add files via upload

---
+++ while_loop.java
@@ -0,0 +1,12 @@
+public class while_loop
+{
+    public static void main(String[] args)
+    {
+        int x=1;
+        while(x<=5)
+        {
+            System.out.println("Hello");
+            x++;
+        }
+    }
+}
