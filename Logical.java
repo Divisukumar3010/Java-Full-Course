e88b1a5c2bb454e99abb27fbd83beafac63c893c
Divi Sukumar <154667008+Divisukumar3010@users.noreply.github.com>
2024-10-05 17:57:55 +0530

    Add files via upload

---
+++ Logical.java
@@ -0,0 +1,10 @@
+class Logical
+{
+    public static void main(String args[])
+    {
+        int a = 10 , b = 20 , c = 30;
+        System.out.println(a<b && b<c);
+        System.out.println(a<b || b<c);
+        System.out.println(!(a<b));
+    }
+}
