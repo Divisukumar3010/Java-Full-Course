6987715324a3bf4c7d2d902c346e3150a23bdd54
Divi Sukumar <154667008+Divisukumar3010@users.noreply.github.com>
2024-10-05 21:26:29 +0530

    Add files via upload

---
+++ IfElse.java
@@ -0,0 +1,15 @@
+class IfElse
+{
+    public static void main(String args[])
+    {
+        int age = 18;
+        if(age >= 18)
+        {
+            System.out.println("You are eligible to vote!");
+        }
+        else
+        {
+            System.out.println("You are not eligible to vote!");
+        }
+    }
+}
