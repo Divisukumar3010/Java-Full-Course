b9b954f1e440938a0117478c48d97336bef2d0a3
Divi Sukumar <154667008+Divisukumar3010@users.noreply.github.com>
2024-10-16 21:16:42 +0530

    Add files via upload

---
+++ Object_Demo.java
@@ -0,0 +1,9 @@
+public class Object_Demo{
+    public void display(){
+        System.out.println("display method");
+    }
+    public static void main(String[] args) {
+        Object_Demo obj = new Object_Demo();
+        obj.display();
+    }
+}
