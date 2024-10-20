0e94191d4cd48a65913ba5f34739d4e9c2425a88
Divi Sukumar <154667008+Divisukumar3010@users.noreply.github.com>
2024-10-20 21:50:33 +0530

    Add files via upload

---
+++ 1d_Array.java
@@ -0,0 +1,12 @@
+public class 1d_Array {
+    public static void main(String[] args) {
+        int arr[] = new int[4];
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 30;
+        arr[3] = 40;
+        for(int i=0;i<4;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
---
+++ TwoD_Array.java
@@ -0,0 +1,18 @@
+public class TwoD_Array {
+    public static void main(String[] args) {
+        int nums[][] = new int[3][4];
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<4;j++){
+                nums[i][j] = (int)(Math.random() *10);
+            }
+        }
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<4;j++){
+                System.out.print(nums[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
