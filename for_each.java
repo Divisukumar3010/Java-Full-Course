1b1e0d9e348c2229587bf891e670e07a9ec80bb1
Divi Sukumar <154667008+Divisukumar3010@users.noreply.github.com>
2024-10-21 21:46:12 +0530

    Add files via upload

---
+++ for_each.java
@@ -0,0 +1,9 @@
+public class for_each {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6};
+
+        for(int n : arr){
+            System.out.println(n);
+        }
+    }
+}
